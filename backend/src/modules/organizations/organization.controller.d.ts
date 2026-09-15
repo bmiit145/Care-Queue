44048c2f28462823d7b4cdff59c71ef9daf7fbc8
bmiit145 <21bmiit145@gmail.com>
2026-09-15 17:26:08 +0530

    remove d.ts

--- backend/src/modules/organizations/organization.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from 'express';
-export declare const createOrganization: (req: Request, res: Response) => Promise<void>;
-export declare const getOrganizations: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=organization.controller.d.ts.map
--- backend/src/modules/services/service.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express';
-import { AuthRequest } from '../../shared/middlewares/auth.middleware';
-export declare const getServices: (req: AuthRequest, res: Response) => Promise<void>;
-export declare const createService: (req: AuthRequest, res: Response) => Promise<void>;
-export declare const getServiceById: (req: AuthRequest, res: Response) => Promise<void>;
-export declare const updateService: (req: AuthRequest, res: Response) => Promise<void>;
-export declare const deleteService: (req: AuthRequest, res: Response) => Promise<void>;
-//# sourceMappingURL=service.controller.d.ts.map
